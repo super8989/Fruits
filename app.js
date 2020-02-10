873c35ffc233c503ec05a8070293044eda3d4276
Sam <super8989@gmail.com>
2020-02-09 22:34:42 -0500

    find all documents

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ client.connect(function(err) {
 
 	const db = client.db(dbName);
 
-	insertDocuments(db, function() {
+	findDocuments(db, function() {
 		client.close();
 	});
 });
@@ -52,4 +52,16 @@ const insertDocuments = function(db, callback) {
 			callback(result);
 		}
 	);
+};
+
+const findDocuments = function(db, callback) {
+	// Get the documents collection
+	const collection = db.collection('fruits');
+	// Find some documents
+	collection.find({}).toArray(function(err, fruits) {
+		assert.equal(err, null);
+		console.log('Found the following records');
+		console.log(fruits);
+		callback(fruits);
+	});
 };
