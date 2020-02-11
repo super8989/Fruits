846bdbb0b46bd9804ce8477c52028ebdb22da5e0
Sam <super8989@gmail.com>
2020-02-10 21:50:37 -0500

    installed mongoose

--- app.js
+++ app.js
@@ -1,58 +1,30 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+//Mongoose
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+mongoose
+	.connect('mongodb://localhost:27017/fruitsDB', {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useCreateIndex: true
+	})
+	.then(() => console.log('DB server connect'))
+	.catch(e => console.log('DB error', e));
 
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-	assert.equal(null, err);
-	console.log('Connected successfully to server');
-
-	const db = client.db(dbName);
-
-	findDocuments(db, function() {
-		client.close();
-	});
+const fruitSchema = new mongoose.Schema({
+	name: String,
+	rating: Number,
+	review: String
 });
 
-const insertDocuments = function(db, callback) {
-	// Get the documents collection
-	const collection = db.collection('fruits');
-	// Insert some documents
-	collection.insertMany(
-		[
-			{
-				name: 'Apple',
-				score: 8,
-				review: 'Great fruit'
-			},
-			{
-				name: 'Orange',
-				score: 6,
-				review: 'Kinda sour'
-			},
-			{
-				name: 'Banana',
-				score: 9,
-				review: 'Great stuff!'
-			}
-		],
-		function(err, result) {
-			assert.equal(err, null);
-			assert.equal(3, result.result.n);
-			assert.equal(3, result.ops.length);
-			console.log('Inserted 3 documents into the collection');
-			callback(result);
-		}
-	);
-};
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+const fruit = new Fruit({
+	name: 'Apple',
+	rating: 7,
+	review: 'Pretty solid as a fruit.'
+});
+
+fruit.save();
 
 const findDocuments = function(db, callback) {
 	// Get the documents collection
